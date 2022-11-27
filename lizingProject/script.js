19c3064d8b3fb214e88ecda21747ef975e646269
Artem-Stryzhakov <Phernander1@gmail.com>
2022-11-27 20:53:59 +0200

    Work has been done.

--- lizingProject/script.js
+++ lizingProject/script.js
@@ -10,27 +10,47 @@ const Inbank_credit = document.querySelector("#Inbank_credit")
 
 const arrayOfBanks = [TFBank_credit, LHV_credit, Liisi_credit, Inbank_credit]
 
-let months;
+const price = parseInt(priceDisplay.innerHTML.split(" ")[0]); // Create array, items in array are ['1549', '€'], then take first parameter (price).
+let months = 18;
+
+// ===== Object with all necessary bank's data ===== //
+const bankData = {
+    interestRate: [0.089, 0.099, 0.076, 0.121] /*Interest rate is just a test value, it might be different...Values in percent are [8.9%, 9,9%, 7,6%, 12,1%]*/,
+    months: [6, 10, 12, 18, 24, 36, 48],
+
+    solveCreditFunctions: {
+        solveTFBankCredit(months) {
+            return ((price / months) + (price * bankData.interestRate[0] / 12)).toFixed(1)
+        },
+        solveLHVCredit(months) {
+            return ((price / months) + (price * bankData.interestRate[1] / 12)).toFixed(1)
+        },
+        solveLiisiCredit(months) {
+            return ((price / months) + (price * bankData.interestRate[2] / 12)).toFixed(1)
+        },
+        solveInbankCredit(months) {
+            return ((price / months) + (price * bankData.interestRate[3] / 12)).toFixed(1)
+        }
+    }
+}
+
 rangeDisplay.addEventListener("change", (event) => {
     const indexRange = event.target.value;
+    months = bankData.months[indexRange]
     labelDisplay.innerHTML = indexRange;
-    if (indexRange == 0) months = 6;
-    if (indexRange == 1) months = 10;
-    if (indexRange == 2) months = 12;
-    if (indexRange == 3) months = 18;
-    if (indexRange == 4) months = 24;
-    if (indexRange == 5) months = 36;
-    if (indexRange == 6) months = 48;
 
-    labelDisplay.innerHTML = `${months} months`
+    labelDisplay.innerHTML = `${bankData.months[indexRange]} months`
     displayCredit()
 })
 
-function displayCredit() {
-    const price =  priceDisplay.innerHTML;
-    const arrayPrice = price.split(" ");
-    const formula = (parseInt(arrayPrice[0]) / months).toFixed(2);
+const solveCreditArray = [
+    bankData.solveCreditFunctions.solveTFBankCredit, bankData.solveCreditFunctions.solveLHVCredit,
+    bankData.solveCreditFunctions.solveLiisiCredit, bankData.solveCreditFunctions.solveInbankCredit
+]
 
-    arrayOfBanks.forEach(items => items.innerHTML = `${formula} € / month`)
-}
+// ===== display credits from other banks ===== //
+const displayCredit = () => arrayOfBanks.forEach((banks, creditBankNum) =>
+    banks.innerHTML = `${solveCreditArray[creditBankNum](months)} € / month`)
 
+displayCredit()
+labelDisplay.innerHTML = `${bankData.months[3]} months`
