7775951324fa32c31b1c8aaae81a646e85df2376
Artem-Stryzhakov <114780369+Artem-Stryzhakov@users.noreply.github.com>
2022-11-28 14:49:09 +0200

    Add files via upload

--- lizingProject/script.js
+++ lizingProject/script.js
@@ -15,23 +15,11 @@ let months = 18;
 
 // ===== Object with all necessary bank's data ===== //
 const bankData = {
-    interestRate: [0.089, 0.099, 0.076, 0.121] /*Interest rate is just a test value, it might be different...Values in percent are [8.9%, 9,9%, 7,6%, 12,1%]*/,
+    interestRate: [0.089, 0.099, 0.076, 0.121],
     months: [6, 10, 12, 18, 24, 36, 48],
 
-    // Need to make some changes here...
     solveCreditFunctions: {
-        solveTFBankCredit(months) {
-            return ((price / months) + (price * bankData.interestRate[0] / 12)).toFixed(1)
-        },
-        solveLHVCredit(months) {
-            return ((price / months) + (price * bankData.interestRate[1] / 12)).toFixed(1)
-        },
-        solveLiisiCredit(months) {
-            return ((price / months) + (price * bankData.interestRate[2] / 12)).toFixed(1)
-        },
-        solveInbankCredit(months) {
-            return ((price / months) + (price * bankData.interestRate[3] / 12)).toFixed(1)
-        }
+        solveBankCredit(months, i) { return ((price / months) + (price * bankData.interestRate[i] / 12)).toFixed(1) }
     }
 }
 
@@ -43,14 +31,9 @@ rangeDisplay.addEventListener("change", (event) => {
     displayCredit()
 })
 
-const solveCreditArray = [
-    bankData.solveCreditFunctions.solveTFBankCredit, bankData.solveCreditFunctions.solveLHVCredit,
-    bankData.solveCreditFunctions.solveLiisiCredit, bankData.solveCreditFunctions.solveInbankCredit
-]
-
 // ===== display credits from other banks ===== //
-const displayCredit = () => arrayOfBanks.forEach((banks, creditBankNum) =>
-    banks.innerHTML = `${solveCreditArray[creditBankNum](months)} € / month`)
+const displayCredit = () => arrayOfBanks.forEach((banks, index) =>
+    banks.innerHTML = `${bankData.solveCreditFunctions.solveBankCredit(months, index)} € / month`)
 
 displayCredit()
 labelDisplay.innerHTML = `${bankData.months[3]} months`
