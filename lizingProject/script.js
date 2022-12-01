af81f0578044b1b23a7b7d64100b643ff98635bf
Artem-Stryzhakov <Phernander1@gmail.com>
2022-12-01 20:10:38 +0200

    Done...

--- lizingProject/script.js
+++ lizingProject/script.js
@@ -1,7 +1,5 @@
 const priceDisplay = document.querySelector("#price")
-
-const labelDisplay = document.querySelector("#lbl")
-const rangeDisplay = document.querySelector("#customRange2")
+const optionDisplay = document.querySelector("#creditSelect")
 
 const TFBank_credit = document.querySelector("#TFBank_credit")
 const LHV_credit = document.querySelector("#LHV_credit")
@@ -10,30 +8,35 @@ const Inbank_credit = document.querySelector("#Inbank_credit")
 
 const arrayOfBanks = [TFBank_credit, LHV_credit, Liisi_credit, Inbank_credit]
 
-const price = parseInt(priceDisplay.innerHTML.split(" ")[0]); // Create array, items in array are ['1549', '€'], then take first parameter (price).
-let months = 18;
+const price = parseInt(priceDisplay.innerHTML.split(" ")[0]);
+let months = 6;
 
 // ===== Object with all necessary bank's data ===== //
 const bankData = {
     interestRate: [0.089, 0.099, 0.076, 0.121],
     months: [6, 10, 12, 18, 24, 36, 48],
+    selectedValues: [],
 
     solveCreditFunctions: {
         solveBankCredit(months, i) { return ((price / months) + (price * bankData.interestRate[i] / 12)).toFixed(1) }
     }
+};
+
+for (const optionDisplayElement of optionDisplay.children) {
+    bankData.selectedValues.push(optionDisplayElement.value)
 }
 
-rangeDisplay.addEventListener("change", (event) => {
-    const indexRange = event.target.value
-    months = bankData.months[indexRange]
-
-    labelDisplay.innerHTML = `${bankData.months[indexRange]} months`
-    displayCredit()
+optionDisplay.addEventListener("change", function() {
+    for (let i = 0; i < bankData.selectedValues.length; i++) {
+        if (optionDisplay.value === bankData.selectedValues[i]) {
+            months = bankData.months[i]
+            displayCredit(months, i);
+        }
+    }
 })
 
 // ===== display credits from other banks ===== //
 const displayCredit = () => arrayOfBanks.forEach((banks, index) =>
     banks.innerHTML = `${bankData.solveCreditFunctions.solveBankCredit(months, index)} € / month`)
 
-displayCredit()
-labelDisplay.innerHTML = `${bankData.months[3]} months`
+displayCredit(months, bankData.interestRate[0])
